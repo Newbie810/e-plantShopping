bdfe34a1dd738176e0b53ae6b7f8f7ca7bf2092e
Anjali Patel <107209808+Newbie810@users.noreply.github.com>
2024-10-02 17:17:22 +0530

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,14 +1,14 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
-import { useSelector, useDispatch } from 'react-redux';
-import { addItem } from './CartSlice'; // Ensure you import the addItem action
-
+import { addItem } from "./CartSlice";
 function ProductList() {
+    const cart = useSelector(state => state.cart.items);
     const [showCart, setShowCart] = useState(false); 
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
-    const cartItems = useSelector((state) => state.cart.items); // Adjust the selector based on your store structure
+    const [plantTypeTotal, setPlantTypeTotal] = useState(0);
     const dispatch = useDispatch();
 
     const plantsArray = [
@@ -218,63 +218,68 @@ function ProductList() {
             ]
         }
     ];
-   const styleObj={
-    backgroundColor: '#4CAF50',
-    color: '#fff!important',
-    padding: '15px',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignIems: 'center',
-    fontSize: '20px',
-   }
-   const styleObjUl={
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '1100px',
-   }
-   const styleA={
-    color: 'white',
-    fontSize: '30px',
-    textDecoration: 'none',
-   }
-   const handleCartClick = (e) => {
-    e.preventDefault();
-    setShowCart(true); // Set showCart to true when cart icon is clicked
-};
-const handlePlantsClick = (e) => {
-    e.preventDefault();
-    setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
-    setShowCart(false); // Hide the cart when navigating to About Us
-};
+    const styleObj={
+        backgroundColor: '#4CAF50',
+        color: '#fff!important',
+        padding: '15px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignIems: 'center',
+        fontSize: '20px',
+    }
+    const styleObjUl={
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '1100px',
+    }
+    const styleA={
+        position: 'relative',
+        color: 'white',
+        fontSize: '30px',
+        textDecoration: 'none',
+    }
+    const styleB={
+        position: 'absolute',
+        left: '28px',
+        top: '23px',
+        fontSize: '20px',
+    }
+    const handleCartClick = (e) => {
+        e.preventDefault();
+        setShowCart(true); // Set showCart to true when cart icon is clicked
+    };
+    const handlePlantsClick = (e) => {
+        e.preventDefault();
+        setShowPlants(true); // Set showAboutUs to true when "About Us" link is clicked
+        setShowCart(false); // Hide the cart when navigating to About Us
+    };
 
-   const handleContinueShopping = (e) => {
-    e.preventDefault();
-    setShowCart(false);
-  };
+    const handleContinueShopping = (e) => {
+        e.preventDefault();
+        setShowCart(false);
+    };
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+        setAddedToCart((prevState) => ({
+            ...prevState,
+            [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+        }));
+    };
 
-  const handleAddToCart = (product) => {
-    console.log(`Adding ${product.name} to cart`);
-    dispatch(addItem({ id: product.id, name: product.name, image: product.image, cost: product.cost }));
-    setAddedToCart((prevState) => ({
-        ...prevState,
-        [product.name]: true,
-    }));
-};
-
-const getTotalQuantity = () => {
-    return cartItems.reduce((total, item) => total + item.quantity, 0);
-};
- 
-
-const CartCount = () => {
-    const cartItems = useSelector(state => state.cart.items);
-  
-    const totalQuantity = cartItems.reduce((total, item) => total + item.quantity, 0);
-  
-    return <div>Cart Items: {totalQuantity}</div>;
-  };
-  
+    useEffect(()=>{
+        let totalQuantity = 0;
+        cart.forEach((item) => {
+            totalQuantity += item.quantity;
+        });
+        setPlantTypeTotal(totalQuantity);
+        
+        let newState = {};
+        cart.forEach(item => {
+            newState[item.name] = true
+        });
+        setAddedToCart(newState);
+    }, [cart]);
 
     return (
         <div>
@@ -293,46 +298,38 @@ const CartCount = () => {
             </div>
             <div style={styleObjUl}>
                 <div> <a href="#" onClick={(e)=>handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                    <p style={styleB}>{plantTypeTotal}</p>
+                    <h1 className='cart'>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>
+                    </h1>
+                </a></div>
             </div>
         </div>
-        <div className="cart-count">
-                <CartCount />
-            </div>
-            {!showCart ? (
-                <div className="product-grid">
-                    {plantsArray.map((category, index) => (
-                        <div key={index}>
-                            <h1>{category.category}</h1>
-                            <div className="product-list">
-                                {category.plants.map((plant, plantIndex) => (
-                                    <div className="product-card" key={plantIndex}>
-                                        <img className="product-image" src={plant.image} alt={plant.name} />
-                                        <div className="product-title">{plant.name}</div>
-                                        <div className="product-description">{plant.description}</div>
-                                        <div className="product-cost">{plant.cost}</div>
-                                        <button
-                                            className="product-button"
-                                            onClick={() => handleAddToCart(plant)}
-                                        >
-                                            {addedToCart[plant.name] ? "Added to Cart" : "Add to Cart"}
-                                        </button>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    ))}
-                    <div>
-                        <button onClick={() => setShowCart(!showCart)}>
-                            {showCart ? "Hide Cart" : "Show Cart"}
-                        </button>
-                        {showCart && <CartItem onContinueShopping={handleContinueShopping} />}
+        {!showCart? (
+        <div className="product-grid">
+            {plantsArray.map((category, index) => (
+            <div key={index}>
+                <h1><div>{category.category}</div></h1>
+                <div className="product-list">
+                    {category.plants.map((plant, plantIndex) => (
+                    <div className="product-card" key={plantIndex}>
+                        <img className="product-image" src={plant.image} alt={plant.name} />
+                        <div className="product-title">{plant.name}</div>
+                        <div className="product-cost">{plant.cost}</div>
+                        <div className="product-description">{plant.description}</div>
+                        {/*Similarly like the above plant.name show other details like description and cost*/}
+                        <button  className={"product-button " + (addedToCart[plant.name] ? "added-to-cart" : "")} onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                     </div>
+                    ))}
                 </div>
-            ) : (
-                <CartItem onContinueShopping={() => setShowCart(false)} />
-            )}
+            </div>
+            ))}
         </div>
+ ) :  (
+    <CartItem onContinueShopping={handleContinueShopping}/>
+)}
+    </div>
     );
 }
 
